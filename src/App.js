bb93c42b8dedc28020d7dc0722d45cc678c9ee17
Muhammad Nur <muhnur7602@gmail.com>
2023-11-05 14:35:25 +0700

    hw12

--- src/App.js
+++ src/App.js
@@ -1,57 +1,114 @@
-
 import * as React from 'react';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { Provider, useDispatch, useSelector } from 'react-redux';
+import { Box, ChakraProvider,Button, Text, VStack, SimpleGrid } from '@chakra-ui/react';
+import { MdRestartAlt } from "react-icons/md";
+
+// Reducer slice
+const ticTacToe = createSlice({
+  name: 'ticTacToe',
+  initialState: {
+    squares: Array(9).fill(null),
+    currentStep: 0,
+    winner: null,
+    nextValue: 'X',
+    status: 'Next player: X',
+  },
+  reducers: {
+    selectSquare(state, action) {
+      if (!state.winner && !state.squares[action.payload]) {
+        const nextSquares = [...state.squares];
+        nextSquares[action.payload] = calculateNextValue(state.squares);
+        const winner = calculateWinner(nextSquares);
+        const nextValue = calculateNextValue(nextSquares);
+        const status = calculateStatus(winner, nextSquares, nextValue);
+        return {
+          squares: nextSquares,
+          winner,
+          nextValue,
+          status
+        };
+      }
+    },
+    restart(state) {
+      const nextSquares = Array(9).fill(null);
+      const winner = calculateWinner(nextSquares);
+      const nextValue = calculateNextValue(nextSquares);
+      const status = calculateStatus(winner, nextSquares, nextValue);
+      return {
+        squares: nextSquares,
+        winner,
+        nextValue,
+        status,
+      };
+    },
+  },
+});
+
+
+export const { selectSquare, restart, jumpToMove } = ticTacToe.actions;
+
+const store = configureStore({
+  reducer: ticTacToe.reducer,
+});
 
 function Board() {
-  const squares = Array(9).fill(null);
-  function selectSquare(square) {
-    const nextSquares = squares.slice();
-    console.log(nextSquares);
-
+  const { status, squares } = useSelector(state => state);
+  const dispatch = useDispatch();
+  function squareHandler(squareIndex) {
+    dispatch(selectSquare(squareIndex));
   }
-
-  function restart() {
-  }
-
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
+      <Button
+        w='10'
+        h='10'
+        colorScheme='linkedin'
+        bgColor='#89CFF3'
+        onClick={() => squareHandler(i)}>
         {squares[i]}
-      </button>
+      </Button>
     );
   }
 
   return (
-    <div>
-      <div >STATUS</div>
-      <div >
+    <VStack mt={4}>
+      <Text fontSize="4xl" fontWeight="bold" mb={6}>
+        {status}
+      </Text>
+      <SimpleGrid columns={3} spacing={2} >
         {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
-      </div>
-      <div >
+      </SimpleGrid>
+      <SimpleGrid columns={3} spacing={2} >
         {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
-      </div>
-      <div >
+      </SimpleGrid>
+      <SimpleGrid columns={3} spacing={2} >
         {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
-      </div>
-      <button onClick={restart}>
-        restart
-      </button>
-    </div>
+      </SimpleGrid>
+    </VStack>
   );
 }
 
 function Game() {
+  const dispatch = useDispatch();
+  function handleRestart() {
+    dispatch(restart());
+  }
   return (
-    <div >
-      <div >
+    <Box bg="#CDF5FD" h="100vh" p={20}>
         <Board />
-      </div>
-    </div>
+        <VStack>
+          <Button leftIcon={<MdRestartAlt/>} size="md" onClick={handleRestart} mt={4} colorScheme="messenger">
+            restart
+          </Button>
+        </VStack>
+    </Box>
   );
 }
 
@@ -91,7 +148,13 @@ function calculateWinner(squares) {
 }
 
 function App() {
-  return <Game />;
+  return (
+    <ChakraProvider>
+      <Provider store={store}>
+        <Game />
+      </Provider>
+    </ChakraProvider>
+  );
 }
 
 export default App;
